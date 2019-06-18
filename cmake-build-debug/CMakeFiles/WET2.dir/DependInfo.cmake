57c0f5306f02ebbba93785a115e13206b52f1ea5
EitanTugy <eitan.tugy@gmail.com>
2019-06-19 01:21:58 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/WET2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/eitan/Desktop/Studies/F/DB/WET2/library2.cpp" "/mnt/c/Users/eitan/Desktop/Studies/F/DB/WET2/cmake-build-debug/CMakeFiles/WET2.dir/library2.cpp.o"
+  "/mnt/c/Users/eitan/Desktop/Studies/F/DB/WET2/main.cpp" "/mnt/c/Users/eitan/Desktop/Studies/F/DB/WET2/cmake-build-debug/CMakeFiles/WET2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/WET2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WET2.dir/main.cpp.o"
+  "CMakeFiles/WET2.dir/library2.cpp.o"
+  "WET2.pdb"
+  "WET2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WET2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
